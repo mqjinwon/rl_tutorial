f00f75ffea777322e50b0015eb309c2f2974a865
mqjinwon <mqjinwon@gmail.com>
2021-06-04 17:57:07 +0900

    make option-critic files

--- algorithms/option_critic.py
+++ algorithms/option_critic.py
@@ -10,9 +10,9 @@ learning_rate = 0.0002
 gamma = 0.98
 n_rollout = 10
 
-class ActorCritic(nn.Module):
+class OptionCritic(nn.Module):
     def __init__(self):
-        super(ActorCritic, self).__init__()
+        super(OptionCritic, self).__init__()
 
         self.data =[]
 
@@ -61,7 +61,6 @@ class ActorCritic(nn.Module):
         
         pi = self.pi(s, softmax_dim=1)
         pi_a = pi.gather(1,a)
-
         loss = -torch.log(pi_a) * delta.detach() + F.smooth_l1_loss(self.v(s), td_target.detach())
 
         self.optimizer.zero_grad()
@@ -81,33 +80,30 @@ class ActorCritic(nn.Module):
 
 
 def main():
-    env = gym.make("CartPole-v1")
-    model = ActorCritic()
+    env = gym.make('CartPole-v1')
+    model = OptionCritic()    
     print_interval = 20
     score = 0.0
 
     for n_epi in range(10000):
         done = False
         s = env.reset()
-
         while not done:
             for t in range(n_rollout):
                 prob = model.pi(torch.from_numpy(s).float())
                 m = Categorical(prob)
                 a = m.sample().item()
                 s_prime, r, done, info = env.step(a)
-                model.put_data((s, a, r, s_prime, done))
-
+                model.put_data((s,a,r,s_prime,done))
+                
                 s = s_prime
                 score += r
-
-                # env.render()
-
+                
                 if done:
-                    break
-
+                    break                     
+            
             model.train_net()
-
+            
         if n_epi%print_interval==0 and n_epi!=0:
             print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
             score = 0.0
