439034795861ffd99d5dd2fe14a3380630ad3067
mqjinwon <mqjinwon@gmail.com>
2021-06-09 22:03:51 +0900

    tmp commit

--- algorithms/option_critic.py
+++ algorithms/option_critic.py
@@ -5,10 +5,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
 
-#Hyperparameters
-learning_rate = 0.0002
-gamma = 0.98
-n_rollout = 10
+from setting import *
+
 
 class OptionCritic(nn.Module):
     def __init__(self):
@@ -16,10 +14,13 @@ class OptionCritic(nn.Module):
 
         self.data =[]
 
-        self.fc1 = nn.Linear(4, 256)
-        self.fc_pi = nn.Linear(256, 2)
-        self.fc_v = nn.Linear(256, 1)
-        self.optimizer = optim.Adam(self.parameters(), lr = learning_rate)
+        self.fc1 = nn.Linear(4,  256)
+
+        # global pi
+        self.global_pi = nn.Linear(256, 2)
+        self.terminate = nn.Linear(256, 1)
+        self.intra_option_policy = nn.Linear(256, 1)
+        self.optimizer = optim.Adam(self.parameters(), lr = IR)
     
     def pi(self, x, softmax_dim=0):
         x = F.relu(self.fc1(x))
@@ -33,30 +34,9 @@ class OptionCritic(nn.Module):
         v = self.fc_v(x)
         return v
 
-    def put_data(self, transition):
-        self.data.append(transition)
-
-    def make_batch(self):
-        s_list, a_list, r_list, s_prime_list, done_list = [], [], [], [], []
-        
-        for transition in self.data:
-            s, a, r, s_prime, done = transition
-            s_list.append(s)
-            a_list.append([a])
-            r_list.append([r/100])
-            s_prime_list.append(s_prime)
-            done_mask = 0.0 if done else 1.0
-            done_list.append([done_mask])
-
-        s_batch, a_batch, r_batch, s_prime_batch, done_batch = torch.tensor(s_list, dtype=torch.float), torch.tensor(a_list), \
-                                                            torch.tensor(r_list, dtype=torch.float), torch.tensor(s_prime_list, dtype=torch.float), \
-                                                            torch.tensor(done_list, dtype=torch.float)
-        self.data = []
-        return s_batch, a_batch, r_batch, s_prime_batch, done_batch
-
     def train_net(self):
         s, a, r, s_prime, done = self.make_batch()
-        td_target = r + gamma * self.v(s_prime) * done
+        td_target = r + GAMMA * self.v(s_prime) * done
         delta = td_target - self.v(s)
         
         pi = self.pi(s, softmax_dim=1)
@@ -88,21 +68,18 @@ def main():
     for n_epi in range(10000):
         done = False
         s = env.reset()
+
+        # choose w according to an epsilon-soft policy over options
+
         while not done:
-            for t in range(n_rollout):
-                prob = model.pi(torch.from_numpy(s).float())
-                m = Categorical(prob)
-                a = m.sample().item()
-                s_prime, r, done, info = env.step(a)
-                model.put_data((s,a,r,s_prime,done))
-                
-                s = s_prime
-                score += r
-                
-                if done:
-                    break                     
-            
-            model.train_net()
+            # choose a according to pi(a|s)
+            prob = model.pi(torch.from_numpy(s).float())
+            m = Categorical(prob)
+            a = m.sample().item()
+
+            #1. Options evalution:
+
+
             
         if n_epi%print_interval==0 and n_epi!=0:
             print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
