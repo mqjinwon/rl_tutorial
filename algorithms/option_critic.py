b13ca54695f7db4c7ace3c1a0e43ebd48dc94a69
mqjinwon <mqjinwon@gmail.com>
2021-06-10 00:14:25 +0900

    fixed errors

--- algorithms/option_critic.py
+++ algorithms/option_critic.py
@@ -9,7 +9,7 @@ import numpy as np
 
 from setting import *
 
-class OptionCritic(nn.module):
+class OptionCritic(nn.Module):
     def __init__(self, n_state, n_action, option_num = 1):
         super(OptionCritic, self).__init__()
 
@@ -17,76 +17,64 @@ class OptionCritic(nn.module):
 
         self.shared_layer = nn.Linear(n_state, 256)
 
-        # policy over options
-        self.policy_over_options = nn.Linear(256, option_num)
-
         # actor parameter
         self.intra_option_policy = []
         self.termination = []
 
         # critic parameter
-        self.Q_omega = []
-        self.Q_u = []
+        self.q_omega = nn.Linear(256, option_num)
+        self.q_u = []
 
         for i in range(option_num):
-            self.intra_option_policy.append(nn.Linear(256, n_action))
-            self.termination.append(nn.Linear(256, 1))
-            self.Q_omega.append(nn.Linear(256, 1))
-            self.Q_u.append(nn.Linear(256, 1))
+            self.intra_option_policy.append(nn.Linear(256, n_action)) # policy
+            self.termination.append(nn.Linear(256, 2))  # terminate or not
+            self.q_u.append(nn.Linear(256, 1))  # value
 
         self.optimizer = optim.Adam(self.parameters(), lr = LR)
     
-    ########################
+    ######################
     ##### model define #####
-    ########################
+    ######################
 
-    def policy_over_option(self, x):
-        x = F.relu(self.shared_layer(x))
-        x = self.policy_over_options(x)
-        prob = F.softmax(x)
+    def policyOverOption(self, x):
+        x = self.qOmega(x)
+        prob = F.softmax(x, dim=0)
 
         return prob
 
-    def intra_option_policy(self, x, option_num=0):
+    def intraOptionPolicy(self, x, option_num=0):
         x = F.relu(self.shared_layer(x))
         x = self.intra_option_policy[option_num](x)
-        prob = F.softmax(x)
+        prob = F.softmax(x, dim=0)
         
         return prob
 
-    def termiation(self, x):
+    def terminationPolicy(self, x , option_num=0):
         x = F.relu(self.shared_layer(x))
-        x = self.termination(x)
-        prob = F.softmax(x)
+        x = self.termination[option_num](x)
+        prob = F.softmax(x, dim=0)
 
         return prob
 
-    def Q_omega(self, x, option_num=0):
-        x = F.relu(self.shared_layer(x))
-        x = self.Q_omega[option_num](x)
+    def qOmega(self, x):
+        x = self.shared_layer(x)
+        x = self.q_omega(x)
 
         return x
 
-    def Q_u(self, x, option_num=0):
-        x = F.relu(self.shared_layer(x))
-        x = self.Q_u[option_num](x)
+    def qU(self, x, option_num=0):
+        x = self.shared_layer(x)
+        x = self.q_u[option_num](x)
 
         return x
 
-    #############################
+    #########################
     ##### Policy evaluation #####
-    #############################
+    #########################
 
-    def cache(self, state, option, action):
-        self.last_state = state
-        self.last_option = option
-        self.last_action = action
-        self.last_Q_omega = self.Q_omega[option](state, option)
-        pass
-
-    def policy_evaluation(self, state, option, action, reward, done, termination):
+    def policyEvaluation(self, s, a, r, s_prime, done, option, termination):
 		# One step target for Q_omega
-        target = reward - self.Q_omega[option](self.last_state, self.last_option)
+        target = r - self.qOmega(self.last_state, self.last_option)
 
         if not done:
             # get best other option
@@ -116,58 +104,32 @@ class OptionCritic(nn.module):
         loss.mean().backward()
         self.optimizer.step()  
 
-    ##############################
+        # def update_Qs(self, state, option, action, reward, done, terminations):
+        #     # One step target for Q_Omega
+        #     target = reward
+        #     if not done:
+        #         beta_omega = terminations[self.last_option].pmf(state)
+        #         target += self.discount * ((1.0 - beta_omega)*self.Q_Omega(state, self.last_option) + \
+        #                     beta_omega*np.max(self.Q_Omega(state)))
+
+        #     # Difference update
+        #     tderror_Q_Omega = target - self.last_Q_Omega
+        #     self.Q_Omega_table[self.last_state, self.last_option] += self.lr * tderror_Q_Omega
+
+        #     tderror_Q_U = target - self.Q_U(self.last_state, self.last_option, self.last_action)
+        #     self.Q_U_table[self.last_state, self.last_option, self.last_action] += self.lr * tderror_Q_U
+
+        #     # Cache
+        #     self.last_state = state
+        #     self.last_option = option
+        #     self.last_action = action
+        #     if not done:
+        #         self.last_Q_Omega = self.Q_Omega(state, option)
+
+
+    ###########################
     ##### Policy improvement #####
-    ##############################
-
-# class OptionCritic(nn.Module):
-    def __init__(self, n_state, n_action, option_num = 1):
-        super(OptionCritic, self).__init__()
-
-        self.data =[]
-
-        self.fc1 = nn.Linear(4,  256)
-
-        # global pi
-        self.global_pi = nn.Linear(256, 2)
-        self.terminate = nn.Linear(256, 1)
-        self.intra_option_policy = nn.Linear(256, 1)
-        self.optimizer = optim.Adam(self.parameters(), lr = IR)
-    
-    def pi(self, x, option_num = 0, softmax_dim=0):
-        x = F.relu(self.fc1(x))
-        x = self.fc_pi[option_num](x)
-        prob = F.softmax(x, dim=softmax_dim)
-        return prob
-        
-
-    def v(self, x, option_num = 0):
-        x = F.relu(self.fc1(x))
-        v = self.fc_v[option_num](x)
-        return v
-
-    def train_net(self):
-        s, a, r, s_prime, done = self.make_batch()
-        td_target = r + GAMMA * self.v(s_prime) * done
-        delta = td_target - self.v(s)
-        
-        pi = self.pi(s, softmax_dim=1)
-        pi_a = pi.gather(1,a)
-        loss = -torch.log(pi_a) * delta.detach() + F.smooth_l1_loss(self.v(s), td_target.detach())
-
-        self.optimizer.zero_grad()
-        loss.mean().backward()
-        self.optimizer.step()  
-        # loss1 = F.smooth_l1_loss(self.v(s), td_target.detach())
-        # loss2 = -torch.log(pi_a) * delta.detach()
-
-        # self.optimizer.zero_grad()
-        # loss1.mean().backward()
-        # self.optimizer.step()    
-
-        # self.optimizer.zero_grad()
-        # loss2.mean().backward()
-        # self.optimizer.step()      
+    ###########################
 
 
 
@@ -177,40 +139,46 @@ def main():
     nstates = env.observation_space.shape[0]
     nactions = env.action_space.n # env.observation_space.shape[0]
 
-    model = OptionCritic(nstates, nactions,4)    
+    model = OptionCritic(nstates, nactions, 2)    
 
+    # each episode
     for n_epi in range(10000):
         done = False
         s = env.reset()
 
-        prob = model.policy_over_option(s)
+        # choose w according to an epsilon-soft policy over options
+        prob = model.policyOverOption(torch.from_numpy(s).float())
         m = Categorical(prob)
         option = m.sample().item()
 
-        prob = model.intra_option_policy(torch.from_numpy(s).float(), option)
-        m = Categorical(prob)
-        a = m.sample().item()
-
-        model.cache(s, option, a)
-
-        # choose w according to an epsilon-soft policy over options
+        # each step
         while not done:
 
             # choose a according to policy_over_option
-            prob = model.intra_option_policy(torch.from_numpy(s).float(), option)
+            prob =  model.intraOptionPolicy(torch.from_numpy(s).float(), option)
             m = Categorical(prob)
             a = m.sample().item()
 
-            s, r, done, info = env.step(a)
-            
-            # if termination in s_prime
+            s_prime, r, done, info = env.step(a)
+
             # choose new w according to an epsilon-soft policy over options
-            if option_terminations[option].sample(state):
-                option = policy_over_options.sample(state)
+            prob = model.terminationPolicy(torch.from_numpy(s_prime).float(), option)
+            m = Categorical(prob)
+            terminate_flag = m.sample().item()
+
+            # if terminate
+            if terminate_flag is 1:
+                prob = model.policyOverOption(torch.from_numpy(s_prime).float())
+                m = Categorical(prob)
+                option = m.sample().item()
+
+            s = s_prime
 
             # 1. option evaluation
 
             # 2. option improvement
+
+            # if termination in s_prime
 
 
 
@@ -220,8 +188,6 @@ def main():
             if done:
                 break                     
             
-            
-
     env.close()
 
     
